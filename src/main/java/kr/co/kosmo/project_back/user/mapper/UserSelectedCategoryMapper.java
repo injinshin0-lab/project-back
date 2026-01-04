0bc1fe1035c9ba3c91bf214257968654ee6856a6
전정현 <hyunee7155@naver.com>
2026-01-04 20:17:25 +0900

    docs: UserSelectedCategoryMapper 주석 추가

--- src/main/java/kr/co/kosmo/project_back/user/mapper/UserSelectedCategoryMapper.java
+++ src/main/java/kr/co/kosmo/project_back/user/mapper/UserSelectedCategoryMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserSelectedCategoryMapper {
-    void insertUserCategories(
+    void insertUserCategories(      // 회원가입 시 선택한 관심분야 저장. 향후 추천 기능에 사용예정
         @Param("userId") Integer userId,
         @Param("categoryIds") List<Integer> categoryIds);
         List<Integer> findUserCategory(Integer userId);
