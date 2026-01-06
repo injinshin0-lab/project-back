5402f99b8e3f7f7fe3aa19ea9598f362d0a06136
junghyun <3847155@gmail.com>
2026-01-06 16:25:32 +0900

    feat: add WebConfig for CORS to connect frontend and backend

---
+++ src/main/java/kr/co/kosmo/project_back/config/WebConfig.java
@@ -0,0 +1,16 @@
+package kr.co.kosmo.project_back.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer  {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
