33c17adc7e62db8d6ba0fd9ac6b64f03f45ccd98
junghyun <3847155@gmail.com>
2026-01-06 16:38:17 +0900

    feat: CORS 설정 추가 (프론트-백엔드 통신 허용)

--- src/main/java/kr/co/kosmo/project_back/config/WebConfig.java
+++ src/main/java/kr/co/kosmo/project_back/config/WebConfig.java
@@ -1,14 +1,16 @@
 package kr.co.kosmo.project_back.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer  {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:8080", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
