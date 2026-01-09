6b70c9b4d32298c883c1a4b6414eb7a885d53af5
junghyun <3847155@gmail.com>
2026-01-09 09:41:52 +0900

    feat: 주소(Address) 조회 API 및 매퍼 구현

--- src/main/java/kr/co/kosmo/project_back/address/mapper/AddressMapper.java
+++ src/main/java/kr/co/kosmo/project_back/address/mapper/AddressMapper.java
@@ -1,10 +1,15 @@
 package kr.co.kosmo.project_back.address.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import kr.co.kosmo.project_back.address.dto.AddressDto;
 
 @Mapper
 public interface AddressMapper {
     void insertAddress(AddressDto address);
+    List<AddressDto> selectAddressByUserId(@Param("userId")Integer userId);
 }
+
