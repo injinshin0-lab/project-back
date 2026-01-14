69438fa77b5fac127e5a1878121ae6defaf128e6
전정현 <hyunee7155@naver.com>
2026-01-14 12:25:57 +0900

    feat: 회원 관심분야 선택 정보 저장 로직 정리

--- src/main/java/kr/co/kosmo/project_back/user/mapper/UserSelectedCategoryMapper.java
+++ src/main/java/kr/co/kosmo/project_back/user/mapper/UserSelectedCategoryMapper.java
@@ -10,6 +10,9 @@ public interface UserSelectedCategoryMapper {
     void insertUserCategories(      // 회원가입 시 선택한 관심분야 저장. 향후 추천 기능에 사용예정
         @Param("userId") Integer userId,
         @Param("categoryIds") List<Integer> categoryIds);
-        List<Integer> findUserCategory(Integer userId);
-        void deleteCategoriesByUser(Integer userId);
+        List<Integer> findUserCategory(@Param("userId") Integer userId);
+        void deleteCategoriesByUser(@Param("userId") Integer userId);
+
+    
+
 }
