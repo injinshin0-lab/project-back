4e05ad214c036dd031b0378ca3d16d740b9ce096
junghyun <3847155@gmail.com>
2026-01-19 17:57:19 +0900

    fix: 리뷰 조회 SQL 및 UserDto 수정

--- src/main/java/kr/co/kosmo/project_back/config/WebConfig.java
+++ src/main/java/kr/co/kosmo/project_back/config/WebConfig.java
@@ -2,6 +2,7 @@ package kr.co.kosmo.project_back.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,5 +15,10 @@ public class WebConfig implements WebMvcConfigurer  {
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+        .addResourceLocations("file:///C:/ai/Workspace/bogam/storage/");
     }
 }
